9c0bdec7bf02b5e0ef972f006946f73fdd3c7b59
HughBailey1 <hughbailey@me.com>
2021-03-11 06:52:26 +0000

    minor bug fixes from an old version

--- main.cs
+++ main.cs
@@ -37,47 +37,47 @@ namespace dhondt_project
         }
 
       
+        public void pMaker(string pname, int pvotes, int rounds, string[] noms)
+        {
+          partyname=pname;
+          votes=pvotes;
+          seats=0;
+          noms=noms;
 
+        }
 
         public int calculations(int v1,int v2,int w1,int w2,int x1,int x2,int y1,int y2,int z1,int z2)
         {
-          
           if(v1 < w1 && v1 < x1 && v1 < y1 && v1 < z1)
           {
             v2 += 1;
             v1 = v1 / (v2 + 1);
             return 1;
           }
-          
-          else if (w1 < v1 && w1 < x1 && w1 < y1 && w1 < z1)
+          else if(w1 < v1 && w1 < x1 && w1 < y1 && w1 < z1)
           { 
             w2 += 1;
             w1 = w1 / (w2 + 1);
             return 2;
           }
-          
-          else if (x1 < v1 && x1 < w1 && x1 < y1 && x1 < z1)
+          else if(x1 < v1 && x1 < w1 && x1 < y1 && x1 < z1)
           {
             x2 += 1;
             x1 = x1 / (x2 + 1);
             return 3;
           }
-          
-          else if (y1 < v1 && y1 < w1 && y1 < x1 && y1 < z1)
+          else if(y1 < v1 && y1 < w1 && y1 < x1 && y1 < z1)
           {
             y2 += 1;
             y1 = y1 / (y2 + 1);
             return 4;
           }
-          
           else if (z1 < v1 && z1 < w1 && z1 < x1 && z1 < y1)
           {
             z2 += 1;
             z1 = z1 / (z2 + 1);
             return 5;
           }
-          else 
-          
           return -1;
         }
 
@@ -87,7 +87,6 @@ namespace dhondt_project
     class Program
     {
         public int rounds = 0;
-        //public int ans = 0;
 
         static void Main(string[] args)
 
@@ -193,44 +192,9 @@ namespace dhondt_project
               
 
             }
-            for (int i = 0; i < 5; i++) 
-            {
-              int ans = Party.calculations(p1.votes(), p1.seats(), p2.votes(), p2.seats(), p3.votes(), p3.seats(), p4.votes(), p4.seats(), p5.votes(), p5.seats());
-              if (ans == 1)
-              {
-                p1.votes() = p1.votes() / p1.seats +1;
-                
-              }
-              else if (ans == 2)
-              {
-                p2.votes() = p2.votes() / p2.seats +1;
-                
-              }
-              if (ans == 3)
-              {
-                p3.votes() = p3.votes() / p3.seats +1;
-                
-              }
-              if (ans == 4)
-              {
-                p4.votes() = p4.votes() / p4.seats +1;
-                
-              }
-              if (ans == 5)
-              {
-                p5.votes() = p5.votes() / p5.seats +1;
-                
-              }
-              //Console.WriteLine(p1.votes);
-              //Console.WriteLine(p2.votes);
-              //Console.WriteLine(p3.votes);
-             // Console.WriteLine(p4.votes);
-              //Console.WriteLine(p5.votes);
 
 
-
-            }
           }
         }
-    }
-//}
+}
+
